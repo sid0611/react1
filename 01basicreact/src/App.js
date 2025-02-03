f73cc8cde975d9aa1ceff2054d9e5d7a22ed6239
Siddharth <chaturvedisiddharth11@gmail.com>
2025-02-03 12:23:38 +0530

    Using Vite & CreateReactApp

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return ( <
+    h1 > Hey all |  < /h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return(
+  <h1>Hey from Vite</h1>
+)
+}
+
+export default App
